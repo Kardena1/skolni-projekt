dc8ceac1661d800c5ec555d9dadb015ad4dbd3d0
Kardena1 <wwtf80539@gmail.com>
2026-02-18 12:31:29 +0100

    Ulozeni vseho do classy

--- program.py
+++ program.py
@@ -1,11 +1,10 @@
 import os
+import sys
 import json
 from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtWidgets import QComboBox
-
-
 
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,43 +13,13 @@ os.chdir(script_dir)
 with open('data.json', 'r', encoding='utf-8') as file: # pro otevreni v cmd je potreba to otevrit absolutni cestou (c:/Users/wwtf8/Desktop/cviceni/zakaznicky_system/zakaznici_upd.py)
     data = json.load(file)   
 
-def pridani_materialu():
-    nazev = input("Zadej nazev materialu: ")
-    bod_tani = int(input("Zadej bod tani: "))
-    bod_varu = int(input("Zadej bod varu: "))
-    merna_kapacita = int(input("Zadejte mernou kapacitu"))
-    novy_material = {
-        "nazev": nazev,
-        "bod_tani": bod_tani,
-        "bod_varu": bod_varu,
-        "merna_kapacita": merna_kapacita
-    }
-    data["material"].append(novy_material)
-    with open('data.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)  # ulozeni zpet do souboru 
+class hlavni_okno(QWidget):
+    def __init__ (self):
+        super().__init__()
 
-
-
-def vypsat_materialy():
-    for i, material in enumerate(data["material"], start=1):
-        print(f"{i}. Material:", material["nazev"], "Bod tani:", material["bod_tani"],"°C", "Bod varu:", material["bod_varu"],"°C","\n") # vypsat materialy s jejich atributy
-
-vypsat_materialy()
-
-# pridani_materialu()
-
-
-# testovaci zona
-
-
-
-
-app = QtWidgets.QApplication([])
-
-hlavni_okno = QtWidgets.QWidget()
-hlavni_okno.setWindowTitle("Test")
-hlavni_okno.setFixedSize(500,500)
-hlavni_okno.setStyleSheet("""
+        self.setWindowTitle("Kalkulacka")
+        self.setFixedSize(500,500)
+        self.setStyleSheet("""
         QPushButton {
             font-size: 18px;
             background-color: #f0f0f0;
@@ -58,54 +27,80 @@ hlavni_okno.setStyleSheet("""
         QLineEdit {
             background-color: #ffffff
         }
-    """)
-
-layout = QtWidgets.QGridLayout()
-
-hlavni_okno.setLayout(layout)
+        """)
 
 
-
-combo = QComboBox()
-
-energie = QtWidgets.QLineEdit()
-vaha = QtWidgets.QLineEdit()
-poc_tepl = QtWidgets.QLineEdit()
-
-debug1 = QtWidgets.QPushButton("Debug")
+        layout = QGridLayout()
+        self.setLayout(layout)
 
 
-layout.addWidget(combo,0,0)
-layout.addWidget(energie,0,1)
-layout.addWidget(vaha,0,2)
-layout.addWidget(poc_tepl,0,3)
-layout.addWidget(debug1,0,4)
+        #TLACITKA -------------------------------------
+
+        self.combo = QComboBox()
+        with open('data.json','r',encoding='UTF-8') as f:
+            data = json.load(f)
+            seznam_nazvu = [m["nazev"] for m in data["material"]]
+            self.combo.addItems(seznam_nazvu)
+
+        self.energie = QtWidgets.QLineEdit()
+        self.vaha = QtWidgets.QLineEdit()
+        self.poc_tepl = QtWidgets.QLineEdit()
+        self.debug1 = QtWidgets.QPushButton("Debug")
+        # eventy
+        self.debug1.clicked.connect(self.debugs)
 
 
-def debugs():
 
-    index = combo.currentIndex()
-    vybrany_material = data["material"][index]
-
-    tani = vybrany_material["bod_tani"]
-    varu = vybrany_material["bod_varu"]
-    kapacita = vybrany_material["merna_kapacita"]
-
-    vysl = tani - varu
-
-    print(vybrany_material["nazev"],tani ,varu,kapacita,vysl, energie.text(),vaha.text(),poc_tepl.text())
+        #layout
+        layout.addWidget(self.combo,0,0)
+        layout.addWidget(self.energie,0,1)
+        layout.addWidget(self.vaha,0,2)
+        layout.addWidget(self.poc_tepl,0,3)
+        layout.addWidget(self.debug1,0,4)
 
 
-debug1.clicked.connect(debugs)
+        # ----------------------------------------------
 
 
-with open('data.json','r',encoding='UTF-8') as f:
-    data = json.load(f)
-    seznam_nazvu = [m["nazev"] for m in data["material"]]
-    combo.addItems(seznam_nazvu)
+    def pridani_materialu(self):
+        nazev = input("Zadej nazev materialu: ")
+        bod_tani = int(input("Zadej bod tani: "))
+        bod_varu = int(input("Zadej bod varu: "))
+        merna_kapacita = int(input("Zadejte mernou kapacitu"))
+        novy_material = {
+            "nazev": nazev,
+            "bod_tani": bod_tani,
+            "bod_varu": bod_varu,
+            "merna_kapacita": merna_kapacita
+        }
+        data["material"].append(novy_material)
+        with open('data.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)  # ulozeni zpet do souboru 
+        
+    def vypsat_materialy(self):
+        for i, material in enumerate(data["material"], start=1):
+            print(f"{i}. Material:", material["nazev"], "Bod tani:", material["bod_tani"],"°C", "Bod varu:", material["bod_varu"],"°C","\n") # vypsat materialy s jejich atributy
 
-hlavni_okno.show()
-app.exec()
+    def debugs(self):
+
+        index = self.combo.currentIndex()
+        vybrany_material = data["material"][index]
+        tani = vybrany_material["bod_tani"]
+        varu = vybrany_material["bod_varu"]
+        kapacita = vybrany_material["merna_kapacita"]
+
+        vysl = tani - varu
+
+        print(f" Nazev materialu: {vybrany_material["nazev"]}\n Bod tani: {tani}\n Bod varu: {varu}\n Merna kapacita: {kapacita}\n Testovy vysledek: {vysl}\n Zadana energie: {self.energie.text()}\n Zadana vaha: {self.vaha.text()}\n Zadana teplota: {self.poc_tepl.text()}\n")
+
+
+
+
+
+app = QApplication(sys.argv)
+okno = hlavni_okno()
+okno.show()
+sys.exit(app.exec())
 
 
 
