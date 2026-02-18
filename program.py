a09e39c2881abb7f03a575d35742660fe0a16a43
Kardena1 <wwtf80539@gmail.com>
2026-02-18 10:03:59 +0100

    Pridani funkce na pridani materialu

--- program.py
+++ program.py
@@ -4,8 +4,33 @@ import json
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-with open('objednavky.json', 'r', encoding='utf-8') as file: # pro otevreni v cmd je potreba to otevrit absolutni cestou (c:/Users/wwtf8/Desktop/cviceni/zakaznicky_system/zakaznici_upd.py)
+with open('data.json', 'r', encoding='utf-8') as file: # pro otevreni v cmd je potreba to otevrit absolutni cestou (c:/Users/wwtf8/Desktop/cviceni/zakaznicky_system/zakaznici_upd.py)
     data = json.load(file)   
+
+def pridani_materialu():
+    nazev = input("Zadej nazev materialu: ")
+    bod_tani = int(input("Zadej bod tani: "))
+    bod_varu = int(input("Zadej bod varu: "))
+    novy_material = {
+        "nazev": nazev,
+        "bod_tani": bod_tani,
+        "bod_varu": bod_varu
+    }
+    data["material"].append(novy_material)
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)  # ulozeni zpet do souboru 
+
+
+
+def vypsat_materialy():
+    for i, material in enumerate(data["material"], start=1):
+        print(f"{i}. Material:", material["nazev"], "Bod tani:", material["bod_tani"],"°C", "Bod varu:", material["bod_varu"],"°C","\n") # vypsat materialy s jejich atributy
+
+vypsat_materialy()
+
+    
+
+
 
 
 
